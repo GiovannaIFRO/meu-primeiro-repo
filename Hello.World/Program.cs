5718f898fd14dfdb586fe53da24252b48cc6bce9
GiovannaIFRO <116303502+GiovannaIFRO@users.noreply.github.com>
2022-10-20 14:54:18 -0400

    Update Program.cs
    retirei o cw

--- Hello.World/Program.cs
+++ Hello.World/Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("----------------------");
+Console.WriteLine("[1] - Saudação. ");
+Console.WriteLine("----------------------");
+
+Console.WriteLine("Escolha uma opção: ");
+int opção = Convert.ToInt32(Console.ReadLine());
+
+if(opção == 1)
+{
+    Console.WriteLine("Qual é o seu nome? ");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine("Olá, " + nome + "!");
+}
